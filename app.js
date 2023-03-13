d69f88108409d2cfa9b24b13e1d16fe075ab7ec5
Nicholas Sanchez <nickasanchez011@gmail.com>
2023-03-13 13:10:59 +0000

    Added .gov

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const pronouns = ['the', 'our'];
 const adjectives = ['great', 'big', 'small', 'awesome', 'cool', 'hot', 'new', 'old', 'fresh', 'best'];
 const nouns = ['jogger', 'racoon', 'website', 'blog', 'startup', 'company', 'business', 'app', 'product', 'service'];
-const extensions = ['.com', '.net', '.us', '.io', '.org', '.co', '.biz', '.info', '.online', '.tech'];
+const extensions = ['.com', '.net', '.us', '.io', '.org', '.co', '.biz', '.info', '.online', '.tech', '.gov'];
 
 for (let i = 0; i < pronouns.length; i++) {
   for (let j = 0; j < adjectives.length; j++) {
